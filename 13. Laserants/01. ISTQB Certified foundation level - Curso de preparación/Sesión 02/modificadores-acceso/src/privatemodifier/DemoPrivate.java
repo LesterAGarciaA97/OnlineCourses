c4c5d2ac8f5becf71c3168af7d298da6255a7c7e
Lester Andres Garcia Aquino <lagarcia2015url@gmail.com>
2022-05-29 17:44:33 -0600

    Adding private modifier missing content

--- 13. Laserants/01. ISTQB Certified foundation level - Curso de preparación/Sesión 02/modificadores-acceso/src/privatemodifier/DemoPrivate.java
+++ 13. Laserants/01. ISTQB Certified foundation level - Curso de preparación/Sesión 02/modificadores-acceso/src/privatemodifier/DemoPrivate.java
@@ -1,13 +1,16 @@
 package privatemodifier;
 
-/*public class DemoPrivate {
+public class DemoPrivate {
     private void mostrar(){
             System.out.println("Hola estoy dentro de la clase PrivateDemo");
         }
-
-        public static void main(String args[]){
+    public static void main(String args[]){
+        DemoPrivate objDemoPrivate = new DemoPrivate();
+        objDemoPrivate.mostrar();
+    }
+        /*public static void main(String args[]){
             DemoPrivate objDemoPrivate = new DemoPrivate();
             objDemoPrivate.mostrar();
         }
-    }
-}*/
+    }*/
+}
