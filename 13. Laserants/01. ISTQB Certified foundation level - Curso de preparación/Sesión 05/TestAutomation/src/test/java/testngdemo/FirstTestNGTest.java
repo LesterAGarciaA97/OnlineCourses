cf5555a0db1cae693bb21875f5c74309cdc83cde
Lester Andres Garcia Aquino <lagarcia2015url@gmail.com>
2022-06-05 16:30:42 -0600

    Agregando contenido sección 05 (UnitTesting, DataProviders, TestingParameters)

---
+++ 13. Laserants/01. ISTQB Certified foundation level - Curso de preparación/Sesión 05/TestAutomation/src/test/java/testngdemo/FirstTestNGTest.java
@@ -0,0 +1,10 @@
+package testngdemo;
+
+import org.testng.annotations.Test;
+
+public class FirstTestNGTest {
+    @Test
+    public void firstTestWithTestng(){
+        System.out.println("First test execution");
+    }
+}
